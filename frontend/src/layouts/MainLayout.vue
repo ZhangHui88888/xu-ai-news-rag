bdbdd4f3380a6b7df2446fe5c5e554a8141ede6c
ZhangHui88888 <harry.zhang592802@gmail.com>
2025-10-17 02:19:28 +0800

    Fronted

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -10,7 +10,7 @@
           :default-active="activeMenu"
           mode="horizontal"
           :ellipsis="false"
-          @select="handleMenuSelect"
+          :router="true"
         >
           <el-menu-item index="/">
             <el-icon><HomeFilled /></el-icon>
@@ -73,15 +73,6 @@ const route = useRoute()
 const userStore = useUserStore()
 
 const activeMenu = computed(() => route.path)
-
-const handleMenuSelect = (index) => {
-  console.log('菜单点击:', index)
-  router.push(index).then(() => {
-    console.log('路由跳转成功:', index)
-  }).catch(err => {
-    console.error('路由跳转失败:', err)
-  })
-}
 
 const handleCommand = (command) => {
   if (command === 'logout') {
