bd8bb64c613d8324676431842371aa1ae8e26578
ZhangHui88888 <harry.zhang592802@gmail.com>
2025-10-18 05:58:05 +0800

    fix

--- backend/src/test/java/com/xu/news/util/VectorStoreTest.java
+++ backend/src/test/java/com/xu/news/util/VectorStoreTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
